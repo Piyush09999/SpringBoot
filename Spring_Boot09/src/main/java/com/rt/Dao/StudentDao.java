aedd4cebde3ff7fb34eebe02d861c73156a9b7a0
Dell <Dell@DESKTOP-T9LRHO3.mshome.net>
2023-08-26 15:26:07 +0530

    Application Property & Dao Add Queries

--- Spring_Boot09/src/main/java/com/rt/Dao/StudentDao.java
+++ Spring_Boot09/src/main/java/com/rt/Dao/StudentDao.java
@@ -1,14 +1,23 @@
 package com.rt.Dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.rt.Entities.Student;
 @Repository
 public interface StudentDao extends JpaRepository<Student, Integer> {
 
-	Student findByName(String name);
+	// Student findByName(String name);
 
-	
+//	@Query(value="select * from studentdata where name=?",nativeQuery = true)
+//	List<Student> stn(String name);
+
+	//JPQL
+	@Query(value = "select s from Student s where s.name=:n", nativeQuery = false)
+	List<Student>stn(@Param("n")String n);
 
 }
